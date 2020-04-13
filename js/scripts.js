dd8791a4bafd13c5679f9d5071472b9a39f266c6
collin9726 <owino.collin@gmail.com>
2020-04-13 12:54:23 +0300

    add toggle effect for small pizza order forms

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".pizza-small").click(function(){
+        $(".pizza-design-small").toggle();
+        $(".pizza-large").toggle();
+        $(".pizza-medium").toggle();
+        $(".pizza-sm-align").toggle();
+    });
+});
